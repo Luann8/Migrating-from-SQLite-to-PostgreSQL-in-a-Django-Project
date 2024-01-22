e7b6d33b863e2337d64d7c7019a3fd4bddc5a571
Luann Pereira Mendes <luannpmendes@gmail.com>
2024-01-22 11:00:54 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,10 @@
 DATABASES = {
-    ‘default’: {
-    ‘ENGINE’: ‘django.db.backends.postgresql_psycopg2’,
-    ‘NAME’: ‘my_db’,
-    ‘USER’ : ‘hero’,
-    ‘PASSWORD’ : ‘my_db@123’,
-    ‘HOST’ : ‘localhost’,
-    ‘PORT’ : ‘5432’,
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'your_db',
+        'USER': 'your_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
